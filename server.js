d5ba905eb518431a5089b392a54db8209be45ef8
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-08-30 21:01:45 +0100

    debug heroku app

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.all('*', (req,res) => {
 
-res.status(200).sendFile(__dirname+'/dist');
+res.status(200).sendFile(__dirname+'/dist/xttreme-frontend/index.html');
 });
 
 // Start the app by listening on the default Heroku port
