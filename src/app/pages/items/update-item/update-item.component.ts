65f95006294638c1f2f51eb480cdfba395f155b0
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-09-20 14:44:56 +0100

    generate update item componenet

---
+++ src/app/pages/items/update-item/update-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-item',
+  templateUrl: './update-item.component.html',
+  styleUrls: ['./update-item.component.scss']
+})
+export class UpdateItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
